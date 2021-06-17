117edffb1260f52834712372f9084a3355968efe
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-17 15:16:37 +0530

    profile card

---
+++ lib/widgets/profile_card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ProfileCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
