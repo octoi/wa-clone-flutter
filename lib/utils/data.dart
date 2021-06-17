a364fb5cc0cbcd167ca06f58aea99c7a09b904a9
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-17 15:55:15 +0530

    profile content class

---
+++ lib/utils/data.dart
@@ -0,0 +1,17 @@
+class ProfileCardContent {
+  final String profile;
+  final String title;
+  final String subTitle;
+  final String time;
+  final String howManyToRead;
+  final bool hasNewMessage;
+
+  ProfileCardContent(
+    this.profile,
+    this.title,
+    this.subTitle,
+    this.time,
+    this.howManyToRead,
+    this.hasNewMessage,
+  );
+}
