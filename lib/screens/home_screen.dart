147ddef9b7ebdfbf93da4461df27d486be928643
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-17 16:14:33 +0530

    generating cards

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:wa_clone/widgets/profile_card.dart';
+import 'package:wa_clone/utils/data.dart';
+import 'package:wa_clone/widgets/create_profile.dart';
 
 class HomeScreen extends StatelessWidget {
+  generateCards() {
+    return profileList.map((profile) {
+      return CreateProfile(content: profile);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       physics: BouncingScrollPhysics(),
       child: Column(
         children: [
-          ProfileCard(
-            profile: "https://i.redd.it/ood9idkaxze51.jpg",
-            title: "John Wick",
-            subTitle: "Hey man how are you",
-            time: "16:19",
-          ),
+          SizedBox(height: 10.0),
+          ...generateCards(),
         ],
       ),
     );
