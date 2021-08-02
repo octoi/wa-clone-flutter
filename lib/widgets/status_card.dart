2028bc6b61ac30919a7ae2096cc8c7589cb9d25c
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-02 17:09:05 +0530

    displayed data

--- lib/widgets/status_card.dart
+++ lib/widgets/status_card.dart
@@ -26,18 +26,44 @@ class StatusCard extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            CircleAvatar(
-              backgroundColor: isSeen ? appLightGreen : appSecondaryTextColor,
-              radius: 30.0,
-              child: Padding(
-                padding: const EdgeInsets.all(3.0),
-                child: CircleAvatar(
+            Row(
+              children: [
+                CircleAvatar(
+                  backgroundColor:
+                      isSeen ? appLightGreen : appSecondaryTextColor,
                   radius: 30.0,
-                  backgroundColor: appSecondaryTextColor,
-                  backgroundImage: NetworkImage(profile),
+                  child: Padding(
+                    padding: const EdgeInsets.all(3.0),
+                    child: CircleAvatar(
+                      radius: 30.0,
+                      backgroundColor: appSecondaryTextColor,
+                      backgroundImage: NetworkImage(profile),
+                    ),
+                  ),
                 ),
-              ),
-            ),
+                SizedBox(width: 20.0),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: appPrimaryTextColor,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    SizedBox(height: 5.0),
+                    Text(
+                      time,
+                      style: TextStyle(
+                        color: appSecondaryTextColor,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            )
           ],
         ),
       ),
