8fee7586ae10dc971c93ad1f91a6418012c55ac0
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-02 17:14:00 +0530

    recent updates status

--- lib/screens/status_screen.dart
+++ lib/screens/status_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wa_clone/utils/constants.dart';
 import 'package:wa_clone/utils/data.dart';
 import 'package:wa_clone/widgets/status_card.dart';
 
@@ -10,13 +11,29 @@ class StatusScreen extends StatelessWidget {
     return SingleChildScrollView(
       physics: BouncingScrollPhysics(),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           StatusCard(
             profile: currentUser.profile,
             title: 'My Status',
             time: '52 minutes ago',
             isMine: true,
-          )
+          ),
+          SizedBox(height: 10.0),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+            child: Text(
+              "Recent Updates",
+              style: TextStyle(
+                fontSize: 17.0,
+                fontWeight: FontWeight.w600,
+                letterSpacing: 0,
+                color: appSecondaryTextColor,
+              ),
+            ),
+          ),
+          SizedBox(height: 10.0),
         ],
       ),
     );
